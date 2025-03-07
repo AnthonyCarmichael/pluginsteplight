3e5b93b6f2b92b966c53b16b3040bc9a3620d625
Anthony Carmichael <anthonycarmichael0@gmail.com>
2025-03-07 11:34:59 -0500

    old version not multithread

--- pluginsteplight/step/stl_stepcreategrid3d.cpp
+++ pluginsteplight/step/stl_stepcreategrid3d.cpp
@@ -62,6 +62,7 @@ void STL_STEPCreateGrid3D::fillPostInputConfigurationDialog(CT_StepConfigurableD
 
 void STL_STEPCreateGrid3D::compute()
 {
+
     using Vec3d                 = Eigen::Vector3d;
     using PointCloudConst       = const CT_AbstractItemDrawableWithPointCloud;
     using PointCloudConstPtr    = PointCloudConst*;
@@ -89,103 +90,60 @@ void STL_STEPCreateGrid3D::compute()
 
         inPointCloud->boundingBox(bbox_bot, bbox_top);
 
+        STL_Grid3D<int>* grid_3d = STL_Grid3D<int>::createGrid3DFromXYZCoords(bbox_bot[0],bbox_bot[1],bbox_bot[2],
+                                                                              bbox_top[0],bbox_top[1],bbox_top[2],
+                                                                              _grid_resolution,
+                                                                              std::numeric_limits<int>::max(),
+                                                                              0);
+        STL_Grid3DBeamVisitor*  visitor =  new STL_Grid3DBeamVisitor(grid_3d);
+        QList<CT_AbstractGrid3DBeamVisitor*> visitorArr;
+        visitorArr.push_back(visitor);
+
+        CT_Grid3DWooTraversalAlgorithm woo(grid_3d,true,visitorArr);
+
         // -----------------------------------------------------------------------------------------------------------------
         // Loop through all points and normals of the input point cloud and start raytracing inside Hough space
-
+        size_t i_point = 0;
         size_t n_points = inPointCloud->pointCloudIndex()->size();
         CT_PointIterator itPoint(inPointCloud->pointCloudIndex());
 
-        //size_t i_point = 0;
-        const unsigned int numThreads = std::thread::hardware_concurrency();
-        const size_t pointsPerThread = n_points / numThreads;
-
-        // Vecteur de threads
-        std::vector<std::future<STL_Grid3D<int>*>> futures;
-        for (unsigned int i = 0; i < numThreads; ++i) {
-            futures.push_back(std::async(std::launch::async, [this, pointsPerThread, i, inPointCloud, inNormalCloud, bbox_bot, bbox_top]() mutable -> STL_Grid3D<int>* {
-                STL_Grid3D<int>* grid_3d = STL_Grid3D<int>::createGrid3DFromXYZCoords(bbox_bot[0],bbox_bot[1],bbox_bot[2],
-                                                                                      bbox_top[0],bbox_top[1],bbox_top[2],
-                                                                                      _grid_resolution,
-                                                                                      std::numeric_limits<int>::max(),
-                                                                                      0);
-                STL_Grid3DBeamVisitor*  visitor =  new STL_Grid3DBeamVisitor(grid_3d);
-                QList<CT_AbstractGrid3DBeamVisitor*> visitorArr;
-                visitorArr.push_back(visitor);
-                CT_Grid3DWooTraversalAlgorithm woo(grid_3d,true,visitorArr);
-
-                // multithreadCompute(pointsPerThread, i, inPointCloud, inNormalCloud, woo);
-                size_t  beginIndex = i * pointsPerThread;
-                CT_PointIterator itPoint(inPointCloud->pointCloudIndex());
-                itPoint.jump(beginIndex);
-                for(size_t i = beginIndex; i < (beginIndex + pointsPerThread); i++)
-                {
-                    // Trouver une façon de gèrer la progress bar
-                    // if( i_point % 100 == 0 )
-                    // {
-                    //     setProgress( static_cast<float>(i_point) * 100.0f / static_cast<float>(n_points) );
-                    // }
-
-                    // Trouver une façon d'arrêter les threads et d'arrêter le compute
-                    // if (isStopped())
-                    // {
-                    //     return;
-                    // }
-
-                    if (isStopped())
-                    {
-                        return nullptr;
-                    }
-
-
-                    itPoint.next();
-                    CT_Point currentPoint = itPoint.currentPoint();
-                    const CT_Normal& currentCTNormal    = inNormalCloud->constNormalAt(i);
-                    Eigen::Vector3d  currentNormal      = currentCTNormal.head(3).cast<double>();
-
-                    float normalLenght = currentNormal.norm();
-
-                    if( normalLenght != 0.0 )
-                    {
-                        currentNormal /= normalLenght;
-                        CT_Beam beam_01(currentPoint, currentNormal);
-                        CT_Beam beam_02(currentPoint, -currentNormal);
-
-                        woo.compute(beam_01);
-                        woo.compute(beam_02);
-                    }
-                }
-
-                delete visitor;
-                return grid_3d;
-            }));
-        }
-
-        if (isStopped())
+        //int* ptrVect = grid_3d->get_data();
+        //int i;
+        //#pragma omp parallel for reduction(+:ptrVect[0:n_points])
+        for(  CT_PointIterator itPoint(inPointCloud->pointCloudIndex());itPoint.hasNext();i_point++ )
         {
-            return;
-        }
+            if( i_point % 100 == 0 )
+            {
+                setProgress( static_cast<float>(i_point) * 100.0f / static_cast<float>(n_points) );
+            }
 
-        STL_Grid3D<int>* grid_3d = nullptr;
-        for (auto &t : futures) {
-            STL_Grid3D<int>* grid = t.get();
+            if( isStopped() )
+            {
+                return ;
+            }
 
-            // if (grid_3d) {
-            //     STL_Grid3D<int>* tmp = grid_3d;
-            //     grid_3d = new STL_Grid3D<int>(*grid_3d + *grid);
-            //     delete tmp;
-            // } else {
-            //     grid_3d = grid;
-            // }
 
-            if (grid_3d) {
-                *grid_3d += *grid;
-                delete grid;
-            } else {
-                grid_3d = grid;
+            const CT_Point&  currentPoint       = itPoint.next().currentPoint();
+            const CT_Normal& currentCTNormal    = inNormalCloud->constNormalAt(i_point);
+            Vec3d      currentNormal      = currentCTNormal.head(3).cast<double>();
+
+            float normalLenght = currentNormal.norm();
+
+            if( normalLenght != 0.0 )
+            {
+                currentNormal /= normalLenght;
+                CT_Beam beam_01( currentPoint, currentNormal );
+                CT_Beam beam_02( currentPoint, -currentNormal );
+
+                woo.compute(beam_01);
+                woo.compute(beam_02);
             }
         }
 
+        delete visitor;
         grid_3d->computeMinMax();
+
+
 
         PS_LOG->addInfoMessage(LogInterface::error, tr("Min value %1").arg(grid_3d->dataMin()));
         PS_LOG->addInfoMessage(LogInterface::error, tr("Max value %1").arg(grid_3d->dataMax()));
